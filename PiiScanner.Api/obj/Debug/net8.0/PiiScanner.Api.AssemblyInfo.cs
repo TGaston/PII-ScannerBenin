07a2eab8fb988efcbdb3441f674d0b8b7763a476
samirale <samir49715@gmail.com>
2025-12-25 15:11:17 +0100

    Ko-fi

--- PiiScanner.Api/obj/Debug/net8.0/PiiScanner.Api.AssemblyInfo.cs
+++ PiiScanner.Api/obj/Debug/net8.0/PiiScanner.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2b4354d349e5c346b4fea08028b186d8d92f61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35605f1773dd8dc628d9607092d7b14625ddcf62")]
 [assembly: System.Reflection.AssemblyProductAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PiiScanner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
